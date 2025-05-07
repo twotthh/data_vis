fa05563652b98a277ff2cb9e6a9b33a617fc47cd
김서현 <122426329+twotthh@users.noreply.github.com>
2025-05-07 11:12:17 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("페이지의 제목")
+
+st.header("첫번째 단락")
+st.subheader("첫번째 단락의 소제목")
+st.write("첫번째 단락")
+
+st.divider()
+
+st.header("두번째 단락")
+st.subheader("두번째 단락의 소제목")
